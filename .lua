591f10389a82191904d96072f655a1bf12ba6524
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-20 20:46:26 +0930

    Update .lua

--- .lua
+++ .lua
@@ -510,7 +510,11 @@ TeleportService:Teleport (Place, Player)
 end
 
 
-
+-- TEST
+function test()
+	if Player.UserID == 3725047752
+		game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("message", "All")
+	end
 
 
 local msg = "**"..Player.Name.."** has successfully logged in"
